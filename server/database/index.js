ce25a915f060b42e0ce1a605ce42681a1ca0bcb6
Gaddour <mannai.abdlkader@gmail.com>
2024-03-11 16:48:38 +0100

    get user's data using only token

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("kanban", "root", "oumeima123", {
+const sequelize = new Sequelize("kanban", "root", "root", {
   host: "localhost",
   dialect: "mysql",
 });
--- server/index.js
+++ server/index.js
@@ -1,48 +1,42 @@
 const express = require("express");
-const sequelize = require("./database/index");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const User = require("./modules/user/model");
-const verifyToken = require('./middelwares/verifyToken.js')
+const verifyToken = require("./middelwares/verifyToken.js");
 const PORT = 3000;
 const app = express();
 
-
 app.use(express.json());
-app.use (express.urlencoded({extended :true}))
 app.use(cors());
 
-app.get("/api/user/mydata", verifyToken , async (req, res) => {
-
-  res.status(201).send (req.userId)
-
-})
+app.get("/api/user/mydata", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.userId);
+    res.send(user);
+  } catch (error) {
+    res.status(404).send("error fetching user");
+  }
+});
 
 app.post("/api/auth/signin", async (req, res) => {
-  
   try {
-    const {username,password}=req.body
-   const user = await User.findOne({
+    const { username, password } = req.body;
+    const user = await User.findOne({
       where: {
-       username,
+        username,
       },
     });
 
-    if(user.password === password){
-      const token = jwt.sign({ id: user.id },
-       "ourSecret",
-        {
-          algorithm: 'HS256',
-          allowInsecureKeySizes: true,
-          expiresIn: 86400, // 24 hours
-        });
-       res.send(token)
+    if (user.password === password) {
+      const token = jwt.sign({ id: user.id }, "ourSecret", {
+        algorithm: "HS256",
+        allowInsecureKeySizes: true,
+        expiresIn: 86400, // 24 hours
+      });
+      res.send(token);
+    } else {
+      res.send("Invalid password");
     }
-    else {
-      res.send("Invalid password")
-    }
-    
-
   } catch (error) {
     res.status(500).json(error);
   }
