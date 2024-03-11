baf82d43dd5b4f93c29f59d8791df488a40de084
Omarboubaker00 <omar.boubakker00@gmail.com>
2024-03-11 13:52:15 +0100

    init project module and define its model and sync it

--- server/database/sync.js
+++ server/database/sync.js
@@ -1,5 +1,6 @@
 const sequelize = require("./");
 const User = require("../modules/user/model");
+const Project = require("../modules/project/model");
 
 (async () => {
   await sequelize.sync({ force: true });
