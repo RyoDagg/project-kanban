f59e3edc27aab91cf663ea6645f5da6fcca9f7c2
Gaddour <mannai.abdlkader@gmail.com>
2024-03-12 21:02:20 +0100

    minor TaskCard font tweak

--- client/src/components/tasks/TaskCard.jsx
+++ client/src/components/tasks/TaskCard.jsx
@@ -5,7 +5,7 @@ const TaskCard = ({ task }) => {
             draggable
             className="bg-white shadow rounded-md border-2 p-3 m-3">
             <h1
-                className="text-[20px] font-[500]"
+                className="text-[17px] font-[500]"
             >
                 {task.name}
             </h1>
