c3e8e3f8255e618251200648f1b897d48e804ba9
Gaddour <mannai.abdlkader@gmail.com>
2024-03-12 19:54:12 +0100

    TaskCard style tweaks

--- client/src/components/tasks/TaskSlot.jsx
+++ client/src/components/tasks/TaskSlot.jsx
@@ -2,15 +2,21 @@ import TaskCard from "./TaskCard"
 
 const TaskSlot = ({ data, title, handleDrop, role }) => {
     return (
-        <div
-            className="bg-[#efefef] mx-2 rounded min-h-[400px]"
+        <div className="mx-2"
             onDrop={(event) => handleDrop(event, role)}
             onDragOver={(event) => event.preventDefault()}
         >
-            <h1 className="text-[30px] bg-[#E9E949] text-center text-[#1F0E71] font-bold">{title}</h1>
-            {data.map((task, i) => (
-                <TaskCard key={i} task={task} />
-            ))}
+            <h1 className="text-[25px] bg-[#E9E949] py-3 shadow rounded px-6 text-[#1F0E71] font-bold">
+                {title}
+            </h1>
+
+            <div
+                className="bg-[#efefef] overflow-y-scroll rounded h-[400px]"
+            >
+                {data.map((task, i) => (
+                    <TaskCard key={i} task={task} />
+                ))}
+            </div>
         </div>
     )
 }
