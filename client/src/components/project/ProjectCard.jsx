8e5bde8f52588792e614e4bc79f50db8b90ac190
lindamnasri <mnasrilinda20@gmail.com>
2024-03-13 23:42:48 +0100

    deletion modal

--- client/src/components/project/ProjectCard.jsx
+++ client/src/components/project/ProjectCard.jsx
@@ -1,17 +1,24 @@
 import { useNavigate } from "react-router-dom";
 
-const ProjectCard = ({ project }) => {
+
+const ProjectCard = ({setProject,project}) => {
     const navigateTo = useNavigate();
 
     return (
         <div
             className="w-[250px] m-[10px] rounded shadow hover:bg-slate-200 cursor-pointer duration-200"
-            onClick={() => {
-                navigateTo("/project", { state: { project } });
-            }}
+            // onClick={() => {
+            //     navigateTo("/project", { state: { project } });
+            // }}
         >
             <h1 className="text-center bg-[#3d1ce335]">{project.name}</h1>
             <div className="h-[150px]"></div>
+          <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#DELETE"
+           onClick={() => {
+                setProject(project)
+            }}
+            >🍄</button>
+           
         </div>
     )
 }
