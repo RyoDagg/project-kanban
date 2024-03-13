4b0c47f8a69446dc06237740cca7b647e73a9766
Gaddour <mannai.abdlkader@gmail.com>
2024-03-13 19:10:03 +0100

    Slot DragOver Visual Effect

--- client/src/components/tasks/TaskSlot.jsx
+++ client/src/components/tasks/TaskSlot.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import TaskCard from "./TaskCard"
 
-const TaskSlot = ({ data, title, handleDrop, role }) => {
+const TaskSlot = ({ data, title, handleDrop, role, }) => {
+    const [dragOver, setDragOver] = useState(false);
+
     return (
         <div className="mx-2"
-            onDrop={(event) => handleDrop(event, role)}
-            onDragOver={(event) => event.preventDefault()}
+            onDrop={(event) => {
+                setDragOver(false)
+                handleDrop(event, role)
+            }}
+            onDragOver={(event) => {
+                setDragOver(true)
+                event.preventDefault()
+            }}
+            onDragLeave={(event) => {
+                setDragOver(false)
+                event.preventDefault()
+            }}
         >
             <h1 className="text-[25px] bg-[#E9E949] py-3 shadow rounded px-6 text-[#1F0E71] font-bold">
                 {title}
             </h1>
 
             <div
-                className="bg-[#efefef] overflow-y-scroll pb-3 rounded min-h-[250px] max-h-[500px]"
+                className={`${dragOver ? "bg-[#d0d0d0]" : "bg-[#efefef]"} overflow-y-scroll pb-3 rounded min-h-[250px] max-h-[500px]`}
             >
                 {data.map((task, i) => (
                     <TaskCard key={i} task={task} />
