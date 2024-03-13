2e828f15a70e590dec7a8af3962b680e5e440f50
Gaddour <mannai.abdlkader@gmail.com>
2024-03-13 08:48:58 +0100

    TaskCard minor tweak

--- client/src/components/tasks/TaskCard.jsx
+++ client/src/components/tasks/TaskCard.jsx
@@ -3,10 +3,8 @@ const TaskCard = ({ task }) => {
         <div
             onDragStart={event => event.dataTransfer.setData("id", task.id)}
             draggable
-            className="bg-white shadow rounded-md border-2 p-3 m-3">
-            <h1
-                className="text-[17px] font-[500]"
-            >
+            className="bg-white shadow rounded-md border-2 p-3 m-3 text-[16px] text-[#202020]">
+            <h1 className="font-[500]">
                 {task.name}
             </h1>
             <p>{task.description}</p>
