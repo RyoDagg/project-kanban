48e3094d51a95e0d4f9a0fde09d273dc3896bd00
Gaddour <mannai.abdlkader@gmail.com>
2024-03-14 10:35:30 +0100

    deletion button

--- client/src/components/project/ProjectCard.jsx
+++ client/src/components/project/ProjectCard.jsx
@@ -1,24 +1,25 @@
 import { useNavigate } from "react-router-dom";
 
 
-const ProjectCard = ({setProject,project}) => {
+const ProjectCard = ({ setProject, project }) => {
     const navigateTo = useNavigate();
 
     return (
         <div
             className="w-[250px] m-[10px] rounded shadow hover:bg-slate-200 cursor-pointer duration-200"
-            onClick={() => {
-                navigateTo("/project", { state: { project } });
-            }}
         >
-            <h1 className="text-center bg-[#3d1ce335]">{project.name}</h1>
+            <h1
+                onClick={() => {
+                    navigateTo("/project", { state: { project } });
+                }}
+                className="text-center bg-[#3d1ce335]">{project.name}</h1>
             <div className="h-[150px]"></div>
-          <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#DELETE"
-           onClick={() => {
-                setProject(project)
-            }}
+            <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#DELETE"
+                onClick={() => {
+                    setProject(project)
+                }}
             >🍄</button>
-           
+
         </div>
     )
 }
