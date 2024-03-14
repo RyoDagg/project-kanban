1e377dfe7d88abd687c965f3de947e987894dcea
OumaymaMansour <mansour.oumeima@gmail.com>
2024-03-14 10:00:58 +0000

    user module refactor

---
+++ server/modules/user/route.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const verifyToken = require("../../middelwares/verifyToken.js");
+const { signOutList, verifySession } = require("../../middelwares/blackList.js");
+
+const userController = require ('./controller')
+
+router.post("/auth/signin",userController.signin);
+router.post("/auth/signup",userController.singnup);
+router.get("/auth/signout",signOutList,userController.signout);
+module.exports = router;
