c6271f2b2548e3ed29242fd8debe444bf2ea99f3
lindamnasri <mnasrilinda20@gmail.com>
2024-03-14 23:02:47 +0100

    edit project #server side

--- server/modules/project/route.js
+++ server/modules/project/route.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const projectController = require("./controller");
+const verifytoken=require("../../middelwares/verifyToken")
 router.route("/")
 .get(projectController.getAll)
-.post(projectController.create)
+.post(verifytoken,projectController.create)
 
 router.route('/:id')
 .get(projectController.getOneProject)
 .delete(projectController.deleteProject)
+.put(projectController.update)
 
 router.route('/:id/tasks')
 .get(projectController.getAllTasks)
