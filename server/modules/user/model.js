8c36e54dc059073ac8d1ef8495b7d6bac23575cb
Gaddour <mannai.abdlkader@gmail.com>
2024-03-17 00:18:57 +0100

    User default image

--- server/modules/user/model.js
+++ server/modules/user/model.js
@@ -2,28 +2,29 @@ const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../../database");
 
 const User = sequelize.define("user", {
-    fullName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    profession: DataTypes.TEXT,
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    photo: {
-        type: DataTypes.STRING,
-    },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  profession: DataTypes.TEXT,
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  photo: {
+    type: DataTypes.STRING,
+    defaultValue: "images/users/Avatar.jpg",
+  },
 });
 
 module.exports = User;
